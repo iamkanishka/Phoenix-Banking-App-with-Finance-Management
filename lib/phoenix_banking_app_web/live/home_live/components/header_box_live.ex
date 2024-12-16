f9576b85dd559a3ba3f8f0220416dc98af4f1659
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-16 21:07:08 +0530

    feat: Header Box component

---
+++ lib/phoenix_banking_app_web/live/home_live/components/header_box_live.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixBankingAppWeb.Home.Components.HeaderBoxLive do
+  use PhoenixBankingAppWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="header-box">
+      <h1 class="header-box-title">
+        {@title} if @type == 'greeting' do
+        <span class="text-bankGradient">
+          &nbsp;{@user}
+        </span>
+        end
+      </h1>
+
+      <p class="header-box-subtext">{@subtext}</p>
+    </div>
+    """
+  end
+end
