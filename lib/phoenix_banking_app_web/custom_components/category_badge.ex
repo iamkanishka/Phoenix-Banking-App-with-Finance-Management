2a237b2a55324e76c1fcd28f52b0cc53e2333706
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-19 21:41:02 +0530

    chore: removed unwanted code

--- lib/phoenix_banking_app_web/custom_components/category_badge.ex
+++ lib/phoenix_banking_app_web/custom_components/category_badge.ex
@@ -5,44 +5,29 @@ defmodule PhoenixBankingAppWeb.CustomComponents.CategoryBadge do
   @impl true
   def render(assigns) do
     ~H"""
-    <%!-- <div class={  "category-badge  #{Map.get(@chip_style, :border_color)} #{@chip_style[:chip_background_color]}" }>
-      <div class={"size-2 rounded-full  #{@chip_style[:background_color]}" }></div>
-       <p class={"text-[12px] font-medium  #{@chip_style.text_color}"}>
-        {@category}
-      </p>
-    </div> --%>
-
-
-    <div class={["category-badge", get_style(@chip_style, :border_color), get_style(@chip_style, :chip_background_color)]}>
+    <div class={[
+      "category-badge",
+      get_style(@chip_style, :border_color),
+      get_style(@chip_style, :chip_background_color)
+    ]}>
       <div class={["size-2 rounded-full", get_style(@chip_style, :background_color)]}></div>
-      <p class={["text-[12px] font-medium", get_style(@chip_style, :text_color)]}><%= @category %></p>
+
+      <p class={["text-[12px] font-medium", get_style(@chip_style, :text_color)]}>{@category}</p>
     </div>
-
-
-    <%!-- <div class={"category-badge #{Map.get(@chip_style, :border_color)}  #{@chip_style[:chip_background_color]}"}>
-      <div class="size-2 rounded-full  bg-green-600" ></div>
-       <p class="text-[12px] font-medium  text-success-700">
-        {@category}
-      </p>
-    </div> --%>
-
     """
   end
 
   @impl true
   def update(assigns, socket) do
     style = TransactionCategoryStyles.get_styles(assigns.category)
-    IO.inspect(style, label: assigns.category)
+
     {:ok,
      socket
      |> assign(:chip_style, style)
      |> assign(assigns)}
   end
 
-
-  def get_style(style,key) do
-
+  def get_style(style, key) do
     Map.get(style, key)
   end
-
 end
