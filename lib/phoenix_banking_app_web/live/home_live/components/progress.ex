d59b730250bf49fa65087b0b2c5aec27fb62bdba
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-21 18:37:05 +0530

    feat: Progress component

---
+++ lib/phoenix_banking_app_web/live/home_live/components/progress.ex
@@ -0,0 +1,22 @@
+defmodule PhoenixBankingAppWeb.HomeLive.Components.Progress do
+  use PhoenixBankingAppWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class={"relative h-4 w-full overflow-hidden rounded-full bg-secondary #{assigns.class}"}>
+      <div
+        class={"h-full w-full flex-1 bg-primary transition-all #{assigns.indicator_class}"}
+        style={"transform: translateX(-#{100 - (@value || 0)}%);"}
+      />
+    </div>
+    """
+  end
+
+  @impl true
+  def update({assigns}, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)}
+  end
+end
