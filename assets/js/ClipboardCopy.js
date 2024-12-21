4537255bd37fc125be74b1ab34226338ba1a8e3c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-21 18:24:04 +0530

    feat: Added CopytoClipboard feature

---
+++ assets/js/ClipboardCopy.js
@@ -0,0 +1,19 @@
+let Hooks = {};
+
+Hooks.ClipboardCopy = {
+  mounted() {
+    this.el.addEventListener("click", () => {
+      const textToCopy = this.el.getAttribute("data-clipboard");
+
+      navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+          console.log("Copied to clipboard!");
+        })
+        .catch(err => {
+          console.error("Failed to copy: ", err);
+        });
+    });
+  }
+};
+
+export default Hooks;
