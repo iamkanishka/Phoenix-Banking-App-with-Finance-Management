be45907206e542c25a919c115a13819a268ddb68
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-23 07:18:13 +0530

    feat: Added  Transaction component with pagination

--- lib/phoenix_banking_app_web/live/transaction_history_live/show.ex
+++ lib/phoenix_banking_app_web/live/transaction_history_live/show.ex
@@ -1,4 +1,15 @@
 defmodule PhoenixBankingAppWeb.TransactionHistoryLive.Show do
 
   use PhoenixBankingAppWeb, :live_view
+
+  @impl true
+  def mount(params, session, socket) do
+  {:ok, socket}
+  end
+
+
+  def format_amount(amount) when is_number(amount) do
+    Number.Currency.number_to_currency(amount, unit: "$", precision: 2)
+  end
+
 end
