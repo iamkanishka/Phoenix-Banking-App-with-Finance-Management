9cf0a69b4427a0b4770dc11dbc44641138d05d03
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-23 07:14:22 +0530

    feat: Bankcard component

---
+++ lib/phoenix_banking_app_web/custom_components/bank_card.ex
@@ -0,0 +1,70 @@
+defmodule PhoenixBankingAppWeb.CustomComponents.BankCard do
+  use PhoenixBankingAppWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="flex flex-col">
+      <.link navigate={"~p/transaction-history/?id=#{@appwrite_item_id}"} class="bank-card">
+        <div class="bank-card_content">
+          <div>
+            <h1 class="text-16 font-semibold text-white">
+              {@account.name}
+            </h1>
+
+            <p class="font-ibm-plex-serif font-black text-white">
+              {format_amount(@account.currentBalance)}
+            </p>
+          </div>
+
+          <article class="flex flex-col gap-2">
+            <div class="flex justify-between">
+              <h1 class="text-12 font-semibold text-white">
+                {@userName}
+              </h1>
+
+              <h2 class="text-12 font-semibold text-white">
+                ●● / ●●
+              </h2>
+            </div>
+
+            <p class="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● <span class="text-16">{@account?.mask}</span>
+            </p>
+          </article>
+        </div>
+
+        <div class="bank-card_icon">
+          <img src="/images/Paypass.svg" width={20} height={24} alt="pay" />
+          <img src="/images/mastercard.svg" width={45} height={32} alt="mastercard" class="ml-5" />
+        </div>
+
+        <img
+          src="/images/lines.png"
+          width={316}
+          height={190}
+          alt="lines"
+          class="absolute top-0 left-0"
+        />
+      </.link>
+
+      <%= if @showBalance do %>
+        <.button phx-hook="ClipboardCopy" data-clipboard={@account?.shareableId}>
+          Copy to Clipboard
+        </.button>
+      <% end %>
+    </div>
+    """
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)}
+  end
+
+  defp format_amount(amount) when is_number(amount) do
+    Number.Currency.number_to_currency(amount, unit: "$", precision: 2)
+  end
+end
