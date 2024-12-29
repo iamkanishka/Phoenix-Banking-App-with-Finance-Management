82af2f2bc4f02846bf64fe819b70704889aa0776
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-29 23:33:17 +0530

    feat: Crypto util module

---
+++ lib/phoenix_banking_app/utils/crypto_util.ex
@@ -0,0 +1,19 @@
+defmodule PhoenixBankingApp.Utils.CryptoUtil do
+  @secret_key_base :crypto.strong_rand_bytes(64) # Typically your Phoenix app's secret_key_base
+
+
+  # Encrypt data
+  def encrypt(data) do
+    Plug.Crypto.MessageEncryptor.encrypt(data, @secret_key_base, "")
+  end
+
+  # Decrypt data
+  def decrypt(encrypted_data) do
+    Plug.Crypto.MessageEncryptor.decrypt(encrypted_data, @secret_key_base, "")
+  end
+end
+
+# Usage
+# encrypted_data = CryptoUtil.encrypt("Hello, Elixir!")
+# {:ok, plaintext} = CryptoUtil.decrypt(encrypted_data)
+# IO.inspect(plaintext) # "Hello, Elixir!"
