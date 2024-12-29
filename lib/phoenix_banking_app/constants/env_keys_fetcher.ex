31371e59e8fce3d043c31047e4201a87b3980762
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-29 23:32:02 +0530

    feat: Environment KeysFetcher

---
+++ lib/phoenix_banking_app/constants/env_keys_fetcher.ex
@@ -0,0 +1,35 @@
+defmodule PhoenixBankingApp.Constants.EnvKeysFetcher do
+  def get_appwrite_database_id() do
+    case Application.get_env(get_app_name(), :appwrite_database_id) do
+      nil ->
+        nil
+
+      database_id ->
+        database_id
+    end
+  end
+
+  def get_user_collection_id() do
+    case Application.get_env(get_app_name(), :appwrite_user_collection_id) do
+      nil ->
+        nil
+
+      appwrite_user_collection_id ->
+        appwrite_user_collection_id
+    end
+  end
+
+  def get_bank_collection_id() do
+    case Application.get_env(get_app_name(), :appwrite_bank_collection_id) do
+      nil ->
+        nil
+
+      appwrite_bank_collection_id ->
+        appwrite_bank_collection_id
+    end
+  end
+
+  defp get_app_name do
+    Mix.Project.config()[:app]
+  end
+end
