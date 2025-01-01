0837e4cdbec1fa409a281c5a7ae3bfd71a7f51e4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-01 21:25:07 +0530

    chore: minor changes

--- lib/phoenix_banking_app_web/live/auth/sign_up.ex
+++ lib/phoenix_banking_app_web/live/auth/sign_up.ex
@@ -1,35 +1,32 @@
 defmodule PhoenixBankingAppWeb.Auth.SignUp do
-alias PhoenixBankingAppWeb.Auth.Services.AuthService
+  alias PhoenixBankingApp.Services.AuthService
   use PhoenixBankingAppWeb, :live_view
-
-
 
   @impl true
   def render(assigns) do
     ~H"""
-      <main class="flex min-h-screen w-full justify-between font-inter">
-    <section class="flex-center size-full max-sm:px-6">
-      <%= if @user == nil do %>
-        <.live_component
-          module={PhoenixBankingAppWeb.Auth.Components.AuthFormLive}
-          id="{:authform}"
-          type="sign-up"
-        />
-      <% else %>
-        <div class="flex flex-col gap-4">
+    <main class="flex min-h-screen w-full justify-between font-inter">
+      <section class="flex-center size-full max-sm:px-6">
+        <%= if @user == nil do %>
           <.live_component
-            module={PhoenixBankingAppWeb.CustomComponents.PlaidLinkLive}
-            id={:plaidlink}
-            user={@user}
-            variant="primary"
-            status={@connect_bank_status}
-             />
-        </div>
-      <% end %>
-    </section>
+            module={PhoenixBankingAppWeb.Auth.Components.AuthFormLive}
+            id="{:authform}"
+            type="sign-up"
+          />
+        <% else %>
+          <div class="flex flex-col gap-4">
+            <.live_component
+              module={PhoenixBankingAppWeb.CustomComponents.PlaidLinkLive}
+              id={:plaidlink}
+              user={@user}
+              variant="primary"
+              status={@connect_bank_status}
+            />
+          </div>
+        <% end %>
+      </section>
 
-
-    <div class="auth-asset">
+      <div class="auth-asset">
         <div>
           <img
             src="/images/auth-image.svg"
@@ -41,7 +38,6 @@ alias PhoenixBankingAppWeb.Auth.Services.AuthService
         </div>
       </div>
     </main>
-
     """
   end
 
@@ -56,25 +52,26 @@ alias PhoenixBankingAppWeb.Auth.Services.AuthService
   @impl true
   def handle_event("plaid_success", %{"public_token" => public_token}, socket) do
     IO.inspect(public_token, label: "Public Token")
-    socket = assign_loader(socket,true)
+    socket = assign_loader(socket, true)
     IO.inspect(socket.assigns.connect_bank_status)
+
     case AuthService.exchange_public_token(public_token, socket) do
       {:ok, result} ->
         IO.inspect(result, label: "exchange_public_token_res")
-        {:noreply, socket
-         |>  assign_loader(false)
 
-        |>  push_navigate(to: "/", replace: true)}
+        {:noreply,
+         socket
+         |> assign_loader(false)
+         |> push_navigate(to: "/", replace: true)}
 
       {:error, reason} ->
         IO.inspect(reason, label: "Exchange Public Token Error")
+
         {:noreply,
-        socket
-        |>  assign_loader(false)
-      }
+         socket
+         |> assign_loader(false)}
     end
   end
-
 
   def handle_info({:user, user}, socket) do
     {:noreply,
@@ -85,5 +82,4 @@ alias PhoenixBankingAppWeb.Auth.Services.AuthService
   defp assign_loader(socket, status) do
     assign(socket, :connect_bank_status, status)
   end
-
 end
