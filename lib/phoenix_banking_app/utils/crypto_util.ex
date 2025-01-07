5a1b3b6b2236dc17836b6f4baacaaa4014c05697
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-07 21:49:19 +0530

    chore: Updated secret and signing keys

--- lib/phoenix_banking_app/utils/crypto_util.ex
+++ lib/phoenix_banking_app/utils/crypto_util.ex
@@ -1,19 +1,36 @@
 defmodule PhoenixBankingApp.Utils.CryptoUtil do
-  @secret_key_base :crypto.strong_rand_bytes(64) # Typically your Phoenix app's secret_key_base
+  @moduledoc """
+  Utility module for encrypting and decrypting data.
+  """
 
+  @key_size 32
+  # 256-bit encryption key
+  @secret_key :crypto.strong_rand_bytes(@key_size)
+  # Separate signing key
+  @signing_key :crypto.strong_rand_bytes(@key_size)
 
-  # Encrypt data
-  def encrypt(data) do
-    Plug.Crypto.MessageEncryptor.encrypt(data, @secret_key_base, @secret_key_base, [])
+  # @key_size 32
+
+  # # Derive encryption and signing keys from your secret_key_base
+  # @secret_key :crypto.hash(:sha256, EnvKeysFetcher.get_secret_key())
+  # @signing_key :crypto.hash(:sha256, "signing_" <> EnvKeysFetcher.get_secret_key())
+
+  @doc """
+  Encrypts the given data using Plug.Crypto.MessageEncryptor.
+  """
+  def encrypt(data) when is_binary(data) do
+    Plug.Crypto.encrypt(data, @secret_key, @signing_key, [])
   end
 
-  # Decrypt data
-  def decrypt(encrypted_data) do
-    Plug.Crypto.MessageEncryptor.decrypt(encrypted_data, @secret_key_base,  @secret_key_base, [])
+  @doc """
+  Decrypts the given encrypted data using Plug.Crypto.MessageEncryptor.
+  """
+  def decrypt(encrypted_data) when is_binary(encrypted_data) do
+    Plug.Crypto.decrypt(encrypted_data, @secret_key, @signing_key, [])
   end
 end
 
-# Usage
-# encrypted_data = CryptoUtil.encrypt("Hello, Elixir!")
-# {:ok, plaintext} = CryptoUtil.decrypt(encrypted_data)
+# Example Usage
+# encrypted_data = PhoenixBankingApp.Utils.CryptoUtil.encrypt("Hello, Elixir!")
+# {:ok, plaintext} = PhoenixBankingApp.Utils.CryptoUtil.decrypt(encrypted_data)
 # IO.inspect(plaintext) # "Hello, Elixir!"
