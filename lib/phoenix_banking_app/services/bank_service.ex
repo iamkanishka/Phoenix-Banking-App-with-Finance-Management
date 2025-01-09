4ca5d2073ed66d75ea03705cb89f6b471cd8943c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-09 07:09:06 +0530

    feat, chore: Added create tranfer functionality minor changes

--- lib/phoenix_banking_app/services/bank_service.ex
+++ lib/phoenix_banking_app/services/bank_service.ex
@@ -1,6 +1,8 @@
 defmodule PhoenixBankingApp.Services.BankService do
+  alias PhoenixBankingApp.Services.TransactionService
+  alias PhoenixBankingApp.Dwolla.Transfer
   alias PhoenixBankingApp.Plaid.Transactions
-  alias PhoenixBankingApp.Plaid.Investments.Transactions.Transaction
+  # alias PhoenixBankingApp.Plaid.Investments.Transactions.Transaction
   alias PhoenixBankingApp.Plaid.Institutions
   alias PhoenixBankingApp.Plaid
   alias Appwrite.Utils.Query
@@ -14,9 +16,9 @@ defmodule PhoenixBankingApp.Services.BankService do
       {:ok, banks} = get_banks(user_id)
 
       accounts =
-        Enum.map(banks, fn bank ->
+        Enum.map(banks["documents"], fn bank ->
           {:ok, accounts_res} =
-            Accounts.get(%{access_token: bank[:access_token]})
+            Accounts.get(%{access_token: bank["access_token"]})
 
           account_data = Enum.at(accounts_res.accounts, 0)
           {:ok, institution} = get_institution(accounts_res.item.institution_id)
@@ -26,13 +28,16 @@ defmodule PhoenixBankingApp.Services.BankService do
             available_balance: account_data.balances.available,
             current_balance: account_data.balances.current,
             institution_id: institution.institution_id,
+            display_name: "#{account_data.name}(#{account_data.balances.available})",
             name: account_data.name,
             official_name: account_data.official_name,
             mask: account_data.mask,
-            type: account_data.typ,
+            type: account_data.type,
             sub_type: account_data.subtype,
+            funding_source: bank["funding_source_id"],
             appwrite_item_id: bank["$id"],
-            sharaeble_id: bank.shareableId
+            sharaeble_id: bank["shareable_id"],
+            user_id: user_id
           }
 
           account
@@ -55,23 +60,32 @@ defmodule PhoenixBankingApp.Services.BankService do
 
   def get_account(appwrite_item_id) do
     try do
-      {:ok, bank} = get_bank(appwrite_item_id)
+      {:ok, bank_data} = get_bank(appwrite_item_id)
+
+
+      IO.inspect(bank_data)
+
+      bank = Enum.at(bank_data["documents"], 0)
+      IO.inspect(bank)
 
       {:ok, accounts_res} =
-        Accounts.get(%{access_token: bank[:access_token]})
+        Accounts.get(%{access_token: bank["access_token"]})
 
       account_data = Enum.at(accounts_res.accounts, 0)
 
-      {:ok, transfer_trasaction_data} = Transaction.get_transactions_by_bank_id(bank["$id"])
+      {:ok, transfer_trasaction_data} =
+        TransactionService.get_transactions_by_bank_id(bank["$id"])
+
+      IO.inspect(transfer_trasaction_data)
 
       transfer_transactions =
-        Enum.map(transfer_trasaction_data, fn transfer_data ->
+        Enum.map(transfer_trasaction_data[:documents], fn transfer_data ->
           %{
             id: transfer_data["$id"],
             name: transfer_data["name"],
-            amount: transfer_data["amount"],
-            date: transfer_data["created_at"],
-            paymentChannel: transfer_data["channel"],
+            amount:  transfer_data["amount"]|> String.to_float() |> trunc(),
+            date: transfer_data["$createdAt"],
+            payment_channel: transfer_data["channel"],
             category: transfer_data["category"],
             type: if(transfer_data["sender_bank_id"] == bank["$id"], do: "debit", else: "credit")
           }
@@ -79,7 +93,11 @@ defmodule PhoenixBankingApp.Services.BankService do
 
       {:ok, institution} = get_institution(accounts_res.item.institution_id)
 
-      {:ok, bank_transactions} = get_transactions(bank[:access_token])
+      IO.inspect(bank["processor_token"])
+      {:ok, bank_transactions} = get_transactions(bank["processor_token"])
+
+      IO.inspect(bank_transactions)
+
 
       account = %{
         id: account_data.account_id,
@@ -89,17 +107,17 @@ defmodule PhoenixBankingApp.Services.BankService do
         name: account_data.name,
         official_name: account_data.official_name,
         mask: account_data.mask,
-        type: account_data.typ,
+        type: account_data.type,
         sub_type: account_data.subtype,
         appwrite_item_id: bank["$id"],
-        sharaeble_id: bank.shareableId
+        sharaeble_id: bank["shareable_id"]
       }
 
       all_transactions =
         (bank_transactions ++ transfer_transactions)
-        |> Enum.sort_by(&Date.from_iso8601!(&1.date), :desc)
+        # |> Enum.sort_by(&Date.from_iso8601!(&1.date), :desc)
 
-     {:ok,  %{account: account, transaction: all_transactions}}
+      {:ok, %{account: account, transaction: all_transactions}}
     catch
       {:error, error} ->
         {:error, error}
@@ -108,13 +126,10 @@ defmodule PhoenixBankingApp.Services.BankService do
 
   def get_institution(institution_id) do
     try do
-      institution =
-        Institutions.get_by_id(%{
-          institution_id: institution_id,
-          country_codes: ["US"]
-        })
-
-      {:ok, institution}
+      Institutions.get_by_id(%{
+        institution_id: institution_id,
+        country_codes: ["US"]
+      })
     catch
       {:error, error} ->
         {:error, error}
@@ -123,14 +138,11 @@ defmodule PhoenixBankingApp.Services.BankService do
 
   def get_banks(user_id) do
     try do
-      banks =
-        Database.list_documents(
-          EnvKeysFetcher.get_appwrite_database_id(),
-          EnvKeysFetcher.get_bank_collection_id(),
-          [Jason.decode!(Query.equal("user_id", [user_id]))]
-        )
-
-      {:ok, banks}
+      Database.list_documents(
+        EnvKeysFetcher.get_appwrite_database_id(),
+        EnvKeysFetcher.get_bank_collection_id(),
+        [Query.equal("user_id", [user_id])]
+      )
     catch
       {:error, error} ->
         {:error, error}
@@ -139,40 +151,38 @@ defmodule PhoenixBankingApp.Services.BankService do
 
   def get_bank(document_id) do
     try do
-      bank =
-        Database.list_documents(
-          EnvKeysFetcher.get_appwrite_database_id(),
-          EnvKeysFetcher.get_bank_collection_id(),
-          [Jason.decode!(Query.equal("$id", [document_id]))]
-        )
-
-      {:ok, bank}
+      Database.list_documents(
+        EnvKeysFetcher.get_appwrite_database_id(),
+        EnvKeysFetcher.get_bank_collection_id(),
+        [Query.equal("$id", [document_id])]
+      )
     catch
       {:error, error} ->
         {:error, error}
     end
   end
 
-  def get_transactions(access_token) do
-    fetch_transactions(access_token, true, [])
-    |> Enum.reverse()
+  def get_transactions(processorToken) do
+    IO.inspect(processorToken, label: "Procerssor Token")
+    transaction_data = fetch_transactions(processorToken, true, []) |> Enum.reverse()
+     {:ok, transaction_data}
   end
 
   defp fetch_transactions(_access_token, false, transactions), do: transactions
 
-  defp fetch_transactions(access_token, true, transactions) do
-    Transactions.sync(%{
-      access_token: access_token,
-      count: 20,
-      cursor: "last-request-cursor-value"
-    })
+  defp fetch_transactions(processorToken, true, transactions) do
+    # Transactions.sync(%{
+    #   access_token: processorToken,
+    #   count: 20
+    # })
 
     case Transactions.sync(%{
-           access_token: access_token,
+         processor_token: processorToken,
            count: 20,
            cursor: "last-request-cursor-value"
          }) do
       {:ok, transactions} ->
+        IO.inspect(transactions)
         new_transactions =
           Enum.map(transactions["added"], fn transaction ->
             %{
@@ -190,7 +200,7 @@ defmodule PhoenixBankingApp.Services.BankService do
           end)
 
         fetch_transactions(
-          access_token,
+          processorToken,
           transactions["has_more"],
           new_transactions ++ transactions
         )
@@ -198,6 +208,47 @@ defmodule PhoenixBankingApp.Services.BankService do
       {:error, error} ->
         IO.puts("An error occurred while getting the transactions: #{inspect(error)}")
         transactions
+    end
+  end
+
+  def create_transfer(token, transfer_params) do
+    try do
+      transfer_request_body = %{
+        _links: %{
+          source: %{
+            href:
+              "https://api-sandbox.dwolla.com/funding-sources/#{transfer_params[:source_funding_source]}"
+          },
+          destination: %{
+            href:
+              "https://api-sandbox.dwolla.com/funding-sources/#{transfer_params[:destination_funding_source]}"
+          }
+        },
+        amount: %{
+          currency: "USD",
+          value: transfer_params[:amount]
+        }
+      }
+
+      IO.inspect(transfer_request_body)
+
+      Transfer.initiate(token, transfer_request_body)
+    catch
+      {:error, error} ->
+        {:error, "Tranfer Process Failed" + error}
+    end
+  end
+
+  def get_bank_by_account_id(account_id) do
+    try do
+      Database.list_documents(
+        EnvKeysFetcher.get_appwrite_database_id(),
+        EnvKeysFetcher.get_bank_collection_id(),
+        [Query.equal("account_id", [account_id])]
+      )
+    catch
+      {:error, error} ->
+        {:error, error}
     end
   end
 end
