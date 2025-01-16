50b6bcb626e989fd5ccb777656d85781973158d9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-16 14:09:34 +0530

    chore: make name and email dynaminc

--- lib/phoenix_banking_app_web/custom_components/footer_live.ex
+++ lib/phoenix_banking_app_web/custom_components/footer_live.ex
@@ -7,19 +7,18 @@ defmodule PhoenixBankingAppWeb.CustomComponents.FooterLive do
     <footer class="footer">
       <div class={"" <> if @type == "mobile", do: "footer_name-mobile", else: "footer_name"}>
         <p class="text-xl font-bold text-gray-700">
-          <%!-- {user?.firstName[0]} --%> K
+          {capitalize_first_letter(@user["first_name"])}
         </p>
       </div>
 
       <div class={"" <> if @type == "mobile", do: "footer_email-mobile", else: "footer_email"}>
         <h1 class="text-14 truncate text-gray-700 font-semibold">
-          <%!-- {user?.firstName} --%>
-         Kanishka
+      {"#{@user["first_name"]} #{@user["last_name"]}"}
         </h1>
 
         <p class="text-14 truncate font-normal text-gray-600">
-          <%!-- {user?.email} --%>
-          kansihkabc1123@gmail.com
+
+          { String.slice("#{@user["email"]}", 0, 20)}...
         </p>
       </div>
 
@@ -33,5 +32,10 @@ defmodule PhoenixBankingAppWeb.CustomComponents.FooterLive do
   @impl true
   def handle_event("handle_log_out", _unsigned_params, socket) do
     {:noreply, socket}
+  end
+
+  def capitalize_first_letter(string) when is_binary(string) do
+    String.slice(string, 0, 1)
+    |> String.upcase()
   end
 end
